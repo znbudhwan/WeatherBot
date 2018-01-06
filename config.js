c09a5376f3ffa16b138a9e4b55c78fb80a7587b5
Zain Budhwani <zbud@ucdavis.edu>
2018-01-05 23:42:53 -0800

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key:         '6tkxnitOORPdGja9fHKgyAQ8k',
-  consumer_secret:      '8CQBAKDNhyz9oFyP69OQismyXeqbNUGYCp4UzSIMGtK2FiZOk7',
-  access_token:         '948473081927905280-WFaousjvOsZhKDDmcbixp58Bn09ab70',
-  access_token_secret:  'EwAXJWPhdKz0IAOUvE8JvW6n4nAKjTFDgIQbqLeZ7tFBO',
-}
